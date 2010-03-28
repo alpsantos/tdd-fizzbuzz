5db60ffcdb5ac56db3969ef440fdfd2b22ff6275
Alessandro <alpsantos@gmail.com>
2010-03-28 02:17:39 -0300

    Initial Commint

---
+++ FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestFizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Check(params int[] numbers)
+        {
+            string returnCheck = string.Empty;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var checkNumber = numbers[i];
+
+                returnCheck += CheckFizzBuzz(checkNumber);
+            }
+
+            return (returnCheck.Length == 0 ? numbers.ToString() : returnCheck.ToString());
+
+        }
+
+        private string CheckFizzBuzz(int checkNumber)
+        {
+            string returnCheck = string.Empty;
+
+            if (IsDivibleFor(checkNumber, 3) && IsDivibleFor(checkNumber, 5))
+                returnCheck = "FizzBuzz";
+            else if (IsDivibleFor(checkNumber, 3) || Contain(checkNumber, 3))
+                returnCheck = "Fizz";
+            else if (IsDivibleFor(checkNumber, 5) || Contain(checkNumber, 5))
+                returnCheck = "Buzz";
+            else
+                returnCheck = checkNumber.ToString();
+
+            return returnCheck;
+        }
+
+        private bool Contain(int checkNumber, int number)
+        {
+            return (checkNumber.ToString().Contains(number.ToString()));
+        }
+
+        private static bool IsDivibleFor(int checkNumber, int divisor)
+        {
+            return checkNumber % divisor == 0;
+        }
+    }
+}
---
+++ TestsFizzBuzz.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestFizzBuzz;
+
+[TestClass]
+public class TestsFizzBuzz
+{
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    [TestMethod]
+    public void ShouldReturn_Fizz_WhenNumberIsDivisibleFor_3()
+    {
+        Assert.AreEqual("Fizz", fizzBuzz.Check(3));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_Buzz_WhenNumberIsDivisibleFor_5()
+    {
+        Assert.AreEqual("Buzz", fizzBuzz.Check(5));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_Fizz_When_SequenceNumbersContains_3()
+    {
+        Assert.AreEqual("Fizz1", fizzBuzz.Check(3,1));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_Buzz_When_SequenceNumbersContains_5()
+    {
+        Assert.AreEqual("1Buzz1", fizzBuzz.Check(1, 5, 1));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_FizzBuzz_When_NumberIsDivisibleFor_3_Or_5()
+    {
+        Assert.AreEqual("FizzBuzz", fizzBuzz.Check(15));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_Fizz_When_NumberContains_3()
+    {
+        Assert.AreEqual("Fizz", fizzBuzz.Check(23));
+    }
+
+    [TestMethod]
+    public void ShouldReturn_Buzz_When_NumberContains_5()
+    {
+        Assert.AreEqual("Buzz", fizzBuzz.Check(55));
+    }
+
+    [TestMethod]
+    public void ShouldReturnBigSequence()
+    {
+        string returnFizzBuzz = fizzBuzz.Check(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30);
+        Assert.AreEqual("12Fizz4BuzzFizz78FizzBuzzFizzBuzz", returnFizzBuzz);
+    }
+
+}
+
